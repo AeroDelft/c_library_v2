deeba01b0b66f6c83e23128713083166088b3e9f
Taneshwar Pranav Parankusam <taneshwar.parankusam@aerodelft.nl>
2022-03-30 14:21:14 +0200

    added mavlink headers and hpp files - new

--- ardupilotmega/testsuite.h
+++ ardupilotmega/testsuite.h
@@ -3101,6 +3101,68 @@ static void mavlink_test_rpm(uint8_t system_id, uint8_t component_id, mavlink_me
 #endif
 }
 
+static void mavlink_test_aerodelft_sensor_temp(uint8_t system_id, uint8_t component_id, mavlink_message_t *last_msg)
+{
+#ifdef MAVLINK_STATUS_FLAG_OUT_MAVLINK1
+    mavlink_status_t *status = mavlink_get_channel_status(MAVLINK_COMM_0);
+        if ((status->flags & MAVLINK_STATUS_FLAG_OUT_MAVLINK1) && MAVLINK_MSG_ID_AERODELFT_SENSOR_TEMP >= 256) {
+            return;
+        }
+#endif
+    mavlink_message_t msg;
+        uint8_t buffer[MAVLINK_MAX_PACKET_LEN];
+        uint16_t i;
+    mavlink_aerodelft_sensor_temp_t packet_in = {
+        93372036854775807ULL,93372036854776311ULL,963498296,157.0
+    };
+    mavlink_aerodelft_sensor_temp_t packet1, packet2;
+        memset(&packet1, 0, sizeof(packet1));
+        packet1.timestamp_sample = packet_in.timestamp_sample;
+        packet1.timestamp = packet_in.timestamp;
+        packet1.device_id = packet_in.device_id;
+        packet1.temperature = packet_in.temperature;
+        
+        
+#ifdef MAVLINK_STATUS_FLAG_OUT_MAVLINK1
+        if (status->flags & MAVLINK_STATUS_FLAG_OUT_MAVLINK1) {
+           // cope with extensions
+           memset(MAVLINK_MSG_ID_AERODELFT_SENSOR_TEMP_MIN_LEN + (char *)&packet1, 0, sizeof(packet1)-MAVLINK_MSG_ID_AERODELFT_SENSOR_TEMP_MIN_LEN);
+        }
+#endif
+        memset(&packet2, 0, sizeof(packet2));
+    mavlink_msg_aerodelft_sensor_temp_encode(system_id, component_id, &msg, &packet1);
+    mavlink_msg_aerodelft_sensor_temp_decode(&msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+
+        memset(&packet2, 0, sizeof(packet2));
+    mavlink_msg_aerodelft_sensor_temp_pack(system_id, component_id, &msg , packet1.timestamp_sample , packet1.timestamp , packet1.device_id , packet1.temperature );
+    mavlink_msg_aerodelft_sensor_temp_decode(&msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+
+        memset(&packet2, 0, sizeof(packet2));
+    mavlink_msg_aerodelft_sensor_temp_pack_chan(system_id, component_id, MAVLINK_COMM_0, &msg , packet1.timestamp_sample , packet1.timestamp , packet1.device_id , packet1.temperature );
+    mavlink_msg_aerodelft_sensor_temp_decode(&msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+
+        memset(&packet2, 0, sizeof(packet2));
+        mavlink_msg_to_send_buffer(buffer, &msg);
+        for (i=0; i<mavlink_msg_get_send_buffer_length(&msg); i++) {
+            comm_send_ch(MAVLINK_COMM_0, buffer[i]);
+        }
+    mavlink_msg_aerodelft_sensor_temp_decode(last_msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+        
+        memset(&packet2, 0, sizeof(packet2));
+    mavlink_msg_aerodelft_sensor_temp_send(MAVLINK_COMM_1 , packet1.timestamp_sample , packet1.timestamp , packet1.device_id , packet1.temperature );
+    mavlink_msg_aerodelft_sensor_temp_decode(last_msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+
+#ifdef MAVLINK_HAVE_GET_MESSAGE_INFO
+    MAVLINK_ASSERT(mavlink_get_message_info_by_name("AERODELFT_SENSOR_TEMP") != NULL);
+    MAVLINK_ASSERT(mavlink_get_message_info_by_id(MAVLINK_MSG_ID_AERODELFT_SENSOR_TEMP) != NULL);
+#endif
+}
+
 static void mavlink_test_device_op_read(uint8_t system_id, uint8_t component_id, mavlink_message_t *last_msg)
 {
 #ifdef MAVLINK_STATUS_FLAG_OUT_MAVLINK1
@@ -4122,6 +4184,7 @@ static void mavlink_test_ardupilotmega(uint8_t system_id, uint8_t component_id, 
     mavlink_test_gopro_set_request(system_id, component_id, last_msg);
     mavlink_test_gopro_set_response(system_id, component_id, last_msg);
     mavlink_test_rpm(system_id, component_id, last_msg);
+    mavlink_test_aerodelft_sensor_temp(system_id, component_id, last_msg);
     mavlink_test_device_op_read(system_id, component_id, last_msg);
     mavlink_test_device_op_read_reply(system_id, component_id, last_msg);
     mavlink_test_device_op_write(system_id, component_id, last_msg);
